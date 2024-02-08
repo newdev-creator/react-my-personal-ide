e9c50991e7972bc6a2527a569e40f49d0283ace9
Julien Freisa <81742220+newdev-creator@users.noreply.github.com>
2024-02-08 15:50:41 +0100

    FEAT: redux

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function CodeTab() {
+  const tabsState = useSelector((state) => state.tabs);
+  console.log(tabsState);
+
   return (
     <textarea
-      spellcheck="false"
+      spellCheck="false"
       className="bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none"
     ></textarea>
   );
