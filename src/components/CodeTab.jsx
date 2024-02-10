7a1d5528f59ba885b7646a9e7db918871b26eb2b
Julien Freisa <81742220+newdev-creator@users.noreply.github.com>
2024-02-10 17:41:29 +0100

    FEAT: function change code

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { updateCode } from "../features/tabs";
 
 export default function CodeTab({ id, code }) {
+  const dispatch = useDispatch();
+
   return (
     <textarea
+      onChange={(e) => dispatch(updateCode({ id, value: e.target.value }))}
       value={code}
       spellCheck="false"
       className="bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none"
